631b1f1d13a4296a7b8b08744b4c1359c952f9c5
mu <cs.21.065@student.uotechnology.edu.iq>
2023-04-01 22:20:23 +0300

    1

--- Services/FightService/IFightService.cs
+++ Services/FightService/IFightService.cs
@@ -1,11 +1,7 @@
-using McU.Dtos;
 using McU.Dtos.Fight;
-using Microsoft.AspNetCore.Mvc;
 
-namespace McU.Services.FightService
-{
-    public interface IFightService
-    {
+namespace McU.Services.FightService{
+    public interface IFightService{
         Task<ServiceResponse<AttackResultDto>> WeaponAttack(WeaponAttackDto request);
         Task<ServiceResponse<AttackResultDto>> SkillAttack(SkillAttackDto request);
         Task<Dictionary<string, object>> Fight(FightRequestDto request);
